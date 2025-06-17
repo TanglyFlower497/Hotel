e3f0eb3bc2b0ee04e19c31ebb982300fdc7444a1
Higor-xz <higormiranda103@gmail.com>
2025-06-17 09:06:36 -0300

    Add files via upload

---
+++ Gerente.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Atividade_hotel;
+
+/**
+ *
+ * @author 12421698650
+ */
+public class Gerente {
+    
+}
