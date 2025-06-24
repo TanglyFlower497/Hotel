3a3b7f9ba3548e991c81fd8bee7f87996a74c2c3
TanglyFlower497 <vcartoone@gmail.com>
2025-06-24 07:16:32 -0300

    Update Reserva.java

--- Reserva.java
+++ Reserva.java
@@ -4,20 +4,29 @@ public class Reserva {
     private String dataSaida;
     private Hospede hospede;
     private Quarto quarto;
-    private Pagamento pagamento;
 
-    public Reserva(String dataEntrada, String dataSaida, Hospede hospede, Quarto quarto) {
+  Reserva(String dataEntrada, String dataSaida, Hospede hospede, Quarto quarto) {
         this.dataEntrada = dataEntrada;
         this.dataSaida = dataSaida;
         this.hospede = hospede;
         this.quarto = quarto;
     }
 
-    public String getDataEntrada() { return dataEntrada; }
-    public String getDataSaida() { return dataSaida; }
-    public Hospede getHospede() { return hospede; }
-    public Quarto getQuarto() { return quarto; }
-    public Pagamento getPagamento() { return pagamento; }
-    public void setPagamento(Pagamento pagamento) { this.pagamento = pagamento; }
+    public String getDataEntrada() {
+        return dataEntrada;
+    }
+
+    public String getDataSaida() {
+        return dataSaida;
+    }
+
+    public Hospede getHospede() {
+        return hospede;
+    }
+
+    public Quarto getQuarto() {
+        return quarto;
+    }
 }
+
 
