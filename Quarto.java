9845272728085655171971230596ab14dbd44a23
TanglyFlower497 <vcartoone@gmail.com>
2025-06-24 07:13:46 -0300

    Update Quarto.java

--- Quarto.java
+++ Quarto.java
@@ -1,17 +1,25 @@
 package Atividade_hotel;
-public class Quarto extends Unidade {
+public class Quarto {
     private int numero;
     private String tipo;
     private double preco;
 
     public Quarto(int numero, String tipo, double preco) {
-        super("Quarto " + numero);
         this.numero = numero;
         this.tipo = tipo;
         this.preco = preco;
     }
 
-    public int getNumero() { return numero; }
-    public String getTipo() { return tipo; }
-    public double getPreco() { return preco; }
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
 }
+
