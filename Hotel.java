792315a6330f7cdab9f51ea1c1d2a154e32ad9d0
TanglyFlower497 <vcartoone@gmail.com>
2025-06-24 07:15:44 -0300

    Update Hotel.java

--- Hotel.java
+++ Hotel.java
@@ -1,18 +1,27 @@
 package Atividade_hotel;
-public class Hotel extends Unidade {
+public class Hotel {
+    private String nome;
     private String cnpj;
     private Endereco endereco;
     private Quarto quarto1;
     private Quarto quarto2;
 
-    public Hotel(String nome, String cnpj, Endereco endereco) {
-        super(nome);
+Hotel(String nome, String cnpj, Endereco endereco) {
+        this.nome = nome;
         this.cnpj = cnpj;
         this.endereco = endereco;
     }
 
-    public void setQuarto1(Quarto q) { this.quarto1 = q; }
-    public void setQuarto2(Quarto q) { this.quarto2 = q; }
-    public Quarto getQuarto1() { return quarto1; }
-    public Quarto getQuarto2() { return quarto2; }
+    public void setQuartos(Quarto q1, Quarto q2) {
+        this.quarto1 = q1;
+        this.quarto2 = q2;
+    }
+
+    public Quarto getQuarto1() {
+        return quarto1;
+    }
+
+    public Quarto getQuarto2() {
+        return quarto2;
+    }
 }
