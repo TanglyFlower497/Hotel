8e890761f7cd9cf5e3364d8a1e08c9ddcdfc9573
TanglyFlower497 <vcartoone@gmail.com>
2025-06-24 07:19:16 -0300

    Update Teste.java

--- Teste.java
+++ Teste.java
@@ -2,68 +2,70 @@ package Atividade_hotel;
 
 import java.util.Scanner;
 
+import java.util.Scanner;
+
 public class Teste {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        Endereco enderecoHotel = new Endereco("Rua das Palmeiras", 123, "São Paulo", "SP", "01234-567");
-        Hotel hotel = new Hotel("Hotel Central", "12.345.678/0001-90", enderecoHotel);
+        Endereco end = new Endereco("Rua das Palmeiras", 123, "São Paulo", "SP", "01234-567");
+        Hotel hotel = new Hotel("Hotel Central", "12.345.678/0001-90", end);
 
-        Quarto quarto1 = new Quarto(101, "Luxo", 200.0);
-        Quarto quarto2 = new Quarto(102, "Simples", 120.0);
-        hotel.setQuarto1(quarto1);
-        hotel.setQuarto2(quarto2);
-        // cadastro de hospede
+        Quarto q1 = new Quarto(101, "Luxo", 200.0);
+        Quarto q2 = new Quarto(102, "Simples", 120.0);
+        hotel.setQuartos(q1, q2);
 
         System.out.println("===== Cadastro de Hóspede =====");
         System.out.print("Nome: ");
-        String nome = scanner.nextLine();
+        String nome = sc.nextLine();
         System.out.print("CPF: ");
-        String cpf = scanner.nextLine();
+        String cpf = sc.nextLine();
         System.out.print("Telefone: ");
-        String telefone = scanner.nextLine();
-        Hospede hospede = new Hospede(nome, cpf, telefone);
-
-        // escolha do quarto
+        String tel = sc.nextLine();
+        Hospede hospede = new Hospede(nome, cpf, tel);
 
         System.out.println("\n===== Escolha o Quarto =====");
-        System.out.println("1 - " + quarto1.getNome() + " - R$" + quarto1.getPreco());
-        System.out.println("2 - " + quarto2.getNome() + " - R$$" + quarto2.getPreco());
+        System.out.println("1 - Quarto " + q1.getNumero() + " - R$" + q1.getPreco());
+        System.out.println("2 - Quarto " + q2.getNumero() + " - R$" + q2.getPreco());
         System.out.print("Opção: ");
-        int opcaoQuarto = scanner.nextInt();
-        scanner.nextLine();
+        int op = sc.nextInt();
+        sc.nextLine();
 
         Quarto quartoSelecionado;
-
-        if (opcaoQuarto == 1) {
-            quartoSelecionado = quarto1;
+        if (op == 1) {
+            quartoSelecionado = q1;
         } else {
-            quartoSelecionado = quarto2;
+            quartoSelecionado = q2;
         }
 
-        System.out.print("Data de entrada (ex: 10/05/2025): ");
-        String dataEntrada = scanner.nextLine();
-        System.out.print("Data de saída (ex: 12/05/2025): ");
-        String dataSaida = scanner.nextLine();
-        Reserva reserva = new Reserva(dataEntrada, dataSaida, hospede, quartoSelecionado);
+        System.out.print("Data de entrada: ");
+        String entrada = sc.nextLine();
+        System.out.print("Data de saída: ");
+        String saida = sc.nextLine();
 
-        System.out.print("Forma de pagamento (Dinheiro, Cartão): ");
-        String formaPagamento = scanner.nextLine();
-        Pagamento pagamento = new Pagamento(quartoSelecionado.getPreco(), formaPagamento);
-        pagamento.setPago(false);
-        reserva.setPagamento(pagamento);
+        Reserva reserva = new Reserva(entrada, saida, hospede, quartoSelecionado);
 
-        // Achei melhor doq colocar um metodo(Exibir infromaçoes)
+        System.out.print("Forma de pagamento: ");
+        String forma = sc.nextLine();
+        Pagamento pagamento = new Pagamento(quartoSelecionado.getPreco(), forma);
+        pagamento.setPago(true);
+
         System.out.println("\n===== RESUMO DA RESERVA =====");
-        System.out.println("Hóspede: " + hospede.getNome());
-        System.out.println("CPF: " + hospede.getCpf());
-        System.out.println("Telefone: " + hospede.getTelefone());
+        Pessoa p = hospede; //polimirfismo
+        p.exibirDados();
         System.out.println("Quarto: " + quartoSelecionado.getNumero() + " - " + quartoSelecionado.getTipo());
         System.out.println("Entrada: " + reserva.getDataEntrada());
         System.out.println("Saída: " + reserva.getDataSaida());
         System.out.println("Valor: R$" + pagamento.getValor());
-        System.out.println("Pagamento: " + pagamento.getFormaPagamento() + " - Pago: " + pagamento.isPago());
-        scanner.close();
-    }
+        System.out.println("Pagamento: " + pagamento.getForma() + " - Pago: " + pagamento.isPago());
 
+        System.out.println("\n===== FUNCIONÁRIOS DO HOTEL =====");
+        Funcionario f1 = new Funcionario("Carlos Silva", "123.456.789-00", "Recepcionista");
+        Gerente g1 = new Gerente("Ana Souza", "987.654.321-00", "Total");
+
+        f1.exibirDados();
+        System.out.println();
+        g1.exibirDados();
+    }
 }
+
